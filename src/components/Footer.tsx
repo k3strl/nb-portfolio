eee3782523f071ea2ba0c4aacd4f8caa07c3cab6
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2025-04-09 11:02:46 -0500

    trying to fix favicon not showing up

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,15 @@ export default function Footer() {
     return (
         <footer>
             <section>
-                <a href="###">Linked In</a>
-                <a href="###">Git Hub</a>
-                <a href="###">BlueSky</a>
+                <li>
+                    <a href="###">Linked In</a>
+                </li>
+                <li>
+                    <a href="###">Git Hub</a>
+                </li>
+                <li>
+                    <a href="###">BlueSky</a>
+                </li>
             </section>
             <section>
                 <p>&copy; 2025 Nic Blaize</p>
