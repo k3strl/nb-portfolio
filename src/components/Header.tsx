fd7ace67f73d0999aa50238266a2c85af6cd9192
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2025-04-14 12:48:18 -0500

    moving onto other more important components, like the actual portfolio :P

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,10 @@
 import Nav from '../components/Nav';
-import Hero from '../components/Hero';
 
 export default function Header() {
-    // return the header code
     return (
         <header>
             <h1>Nic Blaize</h1>
             <Nav />
-            <Hero />
         </header>
     );
 }
---
+++ src/data/projectsData.tsx
@@ -0,0 +1,47 @@
+const projects = [
+    {
+      title: "Pre-Work Study Guide",
+      img: "/assets/projects/study-guide.jpg",
+      desc: "A static, single-page study guide I made while gearing up for coding bootcamp.",
+      pageLink: "https://k3strl.github.io/prework-study-guide/",
+      gitLink: "https://github.com/k3strl/prework-study-guide",
+    },
+    {
+      title: "README Generator",
+      img: "/assets/projects/readme-gen.jpg",
+      desc: "A CLI tool to generate professional README files.",
+      pageLink: "https://github.com/k3strl/CLI-README.md-generator",
+      gitLink: "https://github.com/k3strl/CLI-README.md-generator",
+    },
+    {
+      title: "Salary Tracker",
+      img: "/assets/projects/empl-roster.jpg",
+      desc: "A web application that collects and displays employee and salary data in a table.",
+      pageLink: "https://k3strl.github.io/module3-challenge/",
+      gitLink: "https://github.com/k3strl/module3-challenge",
+    },
+    {
+      title: "Employee Manager",
+      img: "/assets/projects/empl-manager.jpg",
+      desc: "A simple task management app to keep track of your to-dos.",
+      pageLink: "https://github.com/k3strl/employee-manager",
+      gitLink: "https://github.com/k3strl/employee-manager",
+    },
+    {
+      title: "Vehicle Builder",
+      img: "/assets/projects/vehicle-build.jpg",
+      desc: "A simple CLI app that allows the user to build and perform actions on vehicles.",
+      pageLink: "https://k3strl.github.io/prework-study-guide/",
+      gitLink: "https://github.com/k3strl/prework-study-guide",
+    },
+    {
+      title: "Weather Dashboard",
+      img: "/assets/projects/weather-dash.jpg",
+      desc: "A web app to check the weather in various cities.",
+      pageLink: "https://github.com/k3strl/weather-dashboard",
+      gitLink: "https://github.com/k3strl/weather-dashboard",
+    },
+    // Add more projects here
+  ];
+  
+  export default projects;
