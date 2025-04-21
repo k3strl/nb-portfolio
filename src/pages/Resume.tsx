c5b1b6e73a9c152b6e0bcd8940a763b77d856840
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2025-04-21 15:42:49 -0500

    resume added, kinda jank-looking though, need to fix

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -2,7 +2,16 @@
 
 export default function Resume() {
   return (
-    <>
-    </>
+    <div className="resume-container">
+      <iframe
+        src="/resume.html"
+        title="Resume"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 'none',
+        }}
+      />
+    </div>
   );
 }
