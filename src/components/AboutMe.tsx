9572d5a0582fa4fe94152f66d41aedc9c0a4eb12
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2025-04-23 06:11:01 -0500

    reword about-me

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,28 +2,27 @@ const AboutMe = () => {
   return (
     <div className="about-me">
       <h2>Welcome!</h2>
+      <p>I'm Nic, a full stack developer based in Minneapolis, MN. ❄️</p>
       <p>
-        I'm Nic, a full stack developer based in Minneapolis, MN. ❄️
+        I've been using computers since before I have memories: from using them
+        to create art, to building rudimentary static websites with HTML and
+        CSS, to building a second brain with PKM (personal knowledge management)
+        strategies and note-taking apps. But I always felt that programming was
+        out of my reach.
       </p>
       <p>
-        I've been obsessed with computers since I was in high school: from
-        using them to create art, to building rudimentary static websites with
-        HTML and CSS, to building a second brain with PKM (personal knowledge
-        management) strategies and note-taking apps. But I always felt that I
-        wasn't smart enough, or that I didn't begin learning soon enough.
-      </p>
-      <p>
-        {/* Is this too personal? idfk */}
         The best decision I ever made was to sign up for full-stack
         certification through the University of Minnesota. There I was
         encouraged to leave the comfort of my tutorial-hell, to dive in and get
-        my hands dirty. I learned so much so quickly, and made good friends with
-        those in my cohort. I'm eager to jump head-first into my new career,
-        while continuing to learn, grow and improve along the way.
+        my hands dirty. I learned so much so quickly, and good friends and
+        excellent teammates with those in my cohort. I'm eager to use that
+        momentum and jump head-first into my new career, while continuing to
+        learn, grow and improve along the way.
       </p>
       <p>
         Click on the Projects tab above to see what I've built so far. Send me
-        an email or find me elsewhere on the internet by clicking the links below.
+        an email or find me elsewhere on the internet by clicking the links
+        below.
       </p>
     </div>
   );
