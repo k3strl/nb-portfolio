dbd0f3a733295a599f5f873c0e5556a0ffafc4f9
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2025-04-22 19:02:12 -0500

    fixing resume iframe

---
+++ src/components/ResDownload.tsx
@@ -0,0 +1,9 @@
+// const ResDownload = () => {
+//     return (
+//         <div className="resume-download">
+//             <a href="/resume.nic-blaize.pdf" target="_blank" rel="noopener noreferrer">Download a PDF copy!</a>
+//         </div>
+//     );
+// };
+
+// export default ResDownload;
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,18 +1,21 @@
-// import { Link } from "react-router-dom";
-
 export default function Resume() {
   return (
-    <div className="resume-container">
-      <iframe
-        src="/resume.html"
-        title="Resume"
-        style={{
-          width: '60%',
-          height: '60vw',
-          border: 'none',
-          backgroundColor: 'white'
-        }}
-      />
+    <div>
+      <div className="download">
+        <a href="resume.nic-blaize.pdf">Download PDF copy</a>
+      </div>
+      <div className="resume-container">
+        <iframe
+          src="/resume.html"
+          title="Resume"
+          style={{
+            width: "60%",
+            height: "60vw",
+            border: "none",
+            backgroundColor: "white",
+          }}
+        />
+      </div>
     </div>
   );
 }
