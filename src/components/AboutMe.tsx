76a258959ca6a3dd18af1b24954726ca914087a4
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2025-04-22 20:01:26 -0500

    spelling and bug fixes

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,12 +1,12 @@
 const AboutMe = () => {
   return (
     <div className="about-me">
-      <h2>About Me</h2>
+      <h2>Welcome!</h2>
       <p>
-        Welcome! I'm Nic, a full stack developer based in Minneapolis, MN. ❄️
+        I'm Nic, a full stack developer based in Minneapolis, MN. ❄️
       </p>
       <p>
-        I've benen obsessed with computers since I was in high school: from
+        I've been obsessed with computers since I was in high school: from
         using them to create art, to building rudimentary static websites with
         HTML and CSS, to building a second brain with PKM (personal knowledge
         management) strategies and note-taking apps. But I always felt that I
@@ -23,7 +23,7 @@ const AboutMe = () => {
       </p>
       <p>
         Click on the Projects tab above to see what I've built so far. Send me
-        an email or find me on social media by clicking the links below.
+        an email or find me elsewhere on the internet by clicking the links below.
       </p>
     </div>
   );
